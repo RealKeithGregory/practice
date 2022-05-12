3487b3b5257e0e116963a7046d14daa1f39b40ac
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-05-11 23:43:32 -0400

    update

---
+++ arrayPlusArray.js
@@ -0,0 +1,8 @@
+// Write a function to add two arrays
+
+function arrayPlusArray(arr1, arr2) {
+    arr1 = arr1.reduce((a, b) => a + b);
+    arr2 = arr2.reduce((a, b) => a + b);
+    
+    return arr1 + arr2;
+  }
