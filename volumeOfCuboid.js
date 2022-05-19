5a058458067b0cd94091e2549ed65fb6efb3bae5
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-05-18 21:23:02 -0400

    practicing

---
+++ volumeOfCuboid.js
@@ -0,0 +1,8 @@
+// Write a function to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. 
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      let volume = Number(length*width*height)
+      return volume
+    }
+  }
