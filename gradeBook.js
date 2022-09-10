e10854217b9199f6d9e38b4154aa3759bc87077c
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-09-10 09:09:09 -0400

    grade book

--- gradeBook.js
+++ gradeBook.js
@@ -8,7 +8,7 @@
 // 70 <= score < 80	'C'
 // 60 <= score < 70	'D'
 // 0 <= score < 60	'F'
-// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100
 
 function getGrade (s1, s2, s3) {
     let avg = (s1 + s2 + s3)/3;
