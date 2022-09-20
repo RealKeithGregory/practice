b2e0ecbe3dd192cb30244fc2f191279bc45a4d3e
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-09-19 22:05:39 -0400

    hello world function

---
+++ function1HelloWorld.js
@@ -0,0 +1,8 @@
+// Description:
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Write a function "greet" that returns "hello world!"
+let greet = function() {
+    return 'hello world!';
+  };
