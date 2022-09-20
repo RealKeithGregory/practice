fb6bb086ff96ec0ff587ad243c46e0feb5a53317
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-09-20 09:09:09 -0400

    bingo game

--- bingoOrNot.js
+++ bingoOrNot.js
@@ -2,6 +2,7 @@
 
 // Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
 
+
 function bingo(a) {
     if (a.includes(2) && a.includes(7) && a.includes(9) && a.includes(14) && a.includes(15)) {
       return "WIN";
