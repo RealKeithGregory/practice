b66a554a3a1de2604048540f3123f9fa62150f42
RealKeithGregory <KMGIIGITHUB@GMAIL.COM>
2022-12-04 15:19:14 -0500

    remove duplicate letters

---
+++ removeDuplicateLetters.js
@@ -0,0 +1,16 @@
+// You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+// Good luck!
+
+function dup(s) {
+    return s.map(
+      w => w.split('').filter((l,i,a) => l != a[i-1] ).join(''))
+  };
